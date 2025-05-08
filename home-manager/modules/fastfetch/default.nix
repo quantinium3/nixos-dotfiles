bd527c21ffc195e1757d1ce7330b89b840c92dd4
quantinium03 <vaibhavvx331@gmail.com>
2025-05-08 12:08:51 +0300

    declarative fastfetch

--- home-manager/modules/fastfetch/default.nix
+++ home-manager/modules/fastfetch/default.nix
@@ -1,4 +1,3 @@
 {
-  home.file.".config/fastfetch/config.jsonc".source = ./modules/fastfetch/config.jsonc;
-  home.file.".config/fastfetch/art.png".source = ./modules/fastfetch/art.png;
+  home.file.".config/fastfetch/config.jsonc".source = ./config.jsonc;
 }
