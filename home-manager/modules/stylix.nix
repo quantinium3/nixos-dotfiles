15164d47aeacfde5d2833bdd674b72e28700cfc6
quantinium03 <quant@quantinium.dev>
2025-05-22 22:01:31 +0530

    kanagawa scheme

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, config, ... }: {
   imports = [ inputs.stylix.homeManagerModules.stylix ];
 
   home.packages = with pkgs; [
@@ -17,7 +17,24 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa-dragon.yaml";
+    base16Scheme = {
+      base00 = "#0d0c0c";
+      base01 = "#1D1C19";
+      base02 = "#282727";
+      base03 = "#737c73";
+      base04 = "#a6a69c";
+      base05 = "#c5c9c5";
+      base06 = "#7a8382";
+      base07 = "#c5c9c5";
+      base08 = "#c4746e";
+      base09 = "#b98d7b";
+      base0A = "#c4b28a";
+      base0B = "#87a987";
+      base0C = "#8ea4a2";
+      base0D = "#8ba4b0";
+      base0E = "#8992a7";
+      base0F = "#a292a3";
+    };
 
     targets = {
       waybar.enable = false;
