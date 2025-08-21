5c7c10ea7032b260cd3d786660a968acf0f0bb11
quantinium03 <quant@quantinium.dev>
2025-08-22 01:22:21 +0530

    added home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,8 +1,10 @@
-{
-    imports = [
-        ./hardware-configuration.nix
-        ../modules
-    ];
+{ inputs }: {
+  imports = [
+    ./hardware-configuration.nix
+    ../modules
+    inputs.home-manager.nixosModules.default
+  ];
 
-
+  home-manager.backupFileExtension = "backup";
+  nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
 }
