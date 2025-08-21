015a59108fc8f24bf854651a77055cb0b450a9c5
quantinium03 <quant@quantinium.dev>
2025-08-22 02:38:11 +0530

    qt

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -2,5 +2,6 @@
     imports = [
         ./packages
         ./programming
+        ./qt.nix
     ];
 }
---
+++ home-manager/modules/qt.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+
+    style = {
+      package = pkgs.adwaita-qt;
+      name = "adwaita-dark";
+    };
+  };
+}
