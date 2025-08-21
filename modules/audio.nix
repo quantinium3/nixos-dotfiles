5421b400e972fa9178f72d1af3a9e7bef7f7e58c
quantinium03 <quant@quantinium.dev>
2025-08-22 02:38:07 +0530

    audio

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,6 +1,12 @@
 {
   services.pipewire = {
     enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
   };
+
+  security.rtkit.enable = true;
 }
